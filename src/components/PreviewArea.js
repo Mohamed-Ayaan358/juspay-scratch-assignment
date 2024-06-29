ae1f02b64eba0e7839f476352925455051ccbf2a
ayaan <ayaan35813@gmail.com>
2024-06-29 16:07:43 +0530

    History area

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -3,19 +3,41 @@ import CatSprite from "./CatSprite";
 import Select from "react-select";
 
 export default function PreviewArea({ stream }) {
+  const [history, setHistory] = useState([])
 
   return (
     <div className="flex-1 h-full overflow-auto">
-      <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-bold text-gray-700 ml-2 my-2">
-        {" "}
-        {"Preview Area"}{" "}
-      </div>
-      <div className="p-4">
-        <div className="sprites">
-          <CatSprite stream={stream} />
+      <div className="flex-1 h-3/6 overflow-auto">
+        <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-bold text-gray-700 ml-2 my-2">
+          {" "}
+          {"Preview Area"}{" "}
+        </div>
 
+        <div className="p-4">
+          <div className="sprites">
+            <CatSprite stream={stream} modifyHistory={setHistory} />
+
+          </div>
         </div>
       </div>
-    </div>
+      <hr></hr>
+      <div className="p-4">
+        <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-bold text-gray-700 ml-2 my-2">
+          {" "}
+          {"History"}{" "}
+        </div>
+        <span>{history.map(home => <div>{home.key}{home.value}</div>)}</span>
+      </div>
+    </div >
   );
 }
+
+
+{/* <span>{history.map(home => <div>{home.key}{home.value}</div>)}</span> */ }
+{/* <div className="p-4">
+  <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-bold text-gray-700 ml-2 my-2">
+    {" "}
+    {"History"}{" "}
+  </div>
+  <span>{history.map(home => <div>{home.key}{home.value}</div>)}</span>
+</div> */}
