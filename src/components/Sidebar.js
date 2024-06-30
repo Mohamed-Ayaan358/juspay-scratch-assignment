8f00c225f386449ac9636f0465c0eefe448c3203
ayaan <ayaan35813@gmail.com>
2024-06-30 13:18:06 +0530

    Drpdown

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,9 @@
-import React from "react";
-import Icon from "./Icon";
+import React, { useState } from "react";
 import { Draggable } from "react-beautiful-dnd";
 import { Droppable } from "react-beautiful-dnd";
 import { componentSelection } from "./componentSelection";
 
 export default function Sidebar() {
-  const actionList = [
-    "SHIFTX",
-    "SHIFTY",
-    "JUMPTO",
-    "JUMP_TO_X",
-    "JUMP_TO_Y",
-    "JUMPTO_RANDOM",
-    "ROTATE_ANGLE",
-    "TURN_CLOCKWISE",
-    "TURN_COUNTERCLOCKWISE",
-    "TALK",
-    "MODIFY_SIZE",
-    "MODIFY_COLOR",
-    "MODIFY_BG_COLOR",
-    "DEFINE_SIZE",
-    "PAUSE",
-    "LOOP",
-    "START_FLAG",
-
-  ];
   const MotionList = [
     "SHIFTX",
     "SHIFTY",
@@ -49,12 +28,20 @@ export default function Sidebar() {
     "START_FLAG"
   ];
 
+  const [expandedCategory, setExpandedCategory] = useState(null);
+
+  const toggleCategory = (category) => {
+    if (expandedCategory === category) {
+      setExpandedCategory(null);
+    } else {
+      setExpandedCategory(category);
+    }
+  };
 
   return (
     <div className="md:w-60 w-24 flex-none h-full overflow-y-auto flex flex-col items-start p-2 border-r border-gray-200">
       <div className="inline-block bg-blue-400 rounded-full px-3 py-1 text-sm font-bold text-white-700 mr-2 mb-2">
-
-        {"Sidebar"}
+        Sidebar
       </div>
       <Droppable droppableId="block-1" type="actionValue">
         {(value) => (
@@ -63,80 +50,99 @@ export default function Sidebar() {
             ref={value.innerRef}
             className="actionValue my-3"
           >
-            <span className="bg-gray-300 rounded-md p-1 flex items-center">
-              <h1>MOTION</h1>
-            </span>
-            {MotionList.map((item, index) => {
-              return (
-                <Draggable
-                  key={`${item}-actionValue`}
-                  draggableId={`${item}-actionValue`}
-                  index={index}
-                  type="block-1"
-                >
-                  {(value) => (
-                    <ul
-                      ref={value.innerRef}
-                      {...value.draggableProps}
-                      {...value.dragHandleProps}
+            <li className="mt-3">
+              <span
+                className="bg-gray-300 rounded-md p-1 flex items-center cursor-pointer"
+                onClick={() => toggleCategory("motion")}
+              >
+                <h1>MOTION</h1>
+              </span>
+              {expandedCategory === "motion" && (
+                <ul>
+                  {MotionList.map((item, index) => (
+                    <Draggable
+                      key={`${item}-actionValue`}
+                      draggableId={`${item}-actionValue`}
+                      index={index}
+                      type="block-1"
                     >
-                      <div className="my-5"></div>
-                      {componentSelection(item)}
-                    </ul>
-                  )}
-                </Draggable>
-              );
-            })}
-            <span className=" mt-3 bg-gray-300 rounded-md p-1 flex items-center">
-              <h1>LOOKS</h1>
-            </span>
-            {LockList.map((item, index) => {
-              return (
-                <Draggable
-                  key={`${item}-actionValue`}
-                  draggableId={`${item}-actionValue`}
-                  index={index}
-                  type="block-1"
-                >
-                  {(value) => (
-                    <ul
-                      ref={value.innerRef}
-                      {...value.draggableProps}
-                      {...value.dragHandleProps}
+                      {(value) => (
+                        <li
+                          ref={value.innerRef}
+                          {...value.draggableProps}
+                          {...value.dragHandleProps}
+                          className="my-4 ml-4"
+                        >
+                          {componentSelection(item)}
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                </ul>
+              )}
+            </li>
+            <li className="mt-3">
+              <span
+                className="bg-gray-300 rounded-md p-1 flex items-center cursor-pointer"
+                onClick={() => toggleCategory("looks")}
+              >
+                <h1>LOOKS</h1>
+              </span>
+              {expandedCategory === "looks" && (
+                <ul>
+                  {LockList.map((item, index) => (
+                    <Draggable
+                      key={`${item}-actionValue`}
+                      draggableId={`${item}-actionValue`}
+                      index={index}
+                      type="block-1"
                     >
-                      <div className="my-5"></div>
-                      {componentSelection(item)}
-                    </ul>
-                  )}
-                </Draggable>
-              );
-            })}
-            <span className=" mt-3 bg-gray-300 rounded-md p-1 flex items-center">
-              <h1>CONTROL</h1>
-            </span>
-            {ControlList.map((item, index) => {
-              return (
-                <Draggable
-                  key={`${item}-actionValue`}
-                  draggableId={`${item}-actionValue`}
-                  index={index}
-                  type="block-1"
-                >
-                  {(value) => (
-                    <ul
-                      ref={value.innerRef}
-                      {...value.draggableProps}
-                      {...value.dragHandleProps}
+                      {(value) => (
+                        <li
+                          ref={value.innerRef}
+                          {...value.draggableProps}
+                          {...value.dragHandleProps}
+                          className="my-4 ml-4"
+                        >
+                          {componentSelection(item)}
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                </ul>
+              )}
+            </li>
+            <li className="mt-3">
+              <span
+                className="bg-gray-300 rounded-md p-1 flex items-center cursor-pointer"
+                onClick={() => toggleCategory("control")}
+              >
+                <h1>CONTROL</h1>
+              </span>
+              {expandedCategory === "control" && (
+                <ul>
+                  {ControlList.map((item, index) => (
+                    <Draggable
+                      key={`${item}-actionValue`}
+                      draggableId={`${item}-actionValue`}
+                      index={index}
+                      type="block-1"
                     >
-                      <div className="my-5"></div>
-                      {componentSelection(item)}
-                    </ul>
-                  )}
-                </Draggable>
-              );
-            })}
-
-            {/* {value.placeholder} */}
+                      {(value) => (
+                        <li
+                          ref={value.innerRef}
+                          {...value.draggableProps}
+                          {...value.dragHandleProps}
+                          className="my-4 ml-4"
+                        >
+                          {componentSelection(item)}
+                        </li>
+                      )}
+                    </Draggable>
+                  ))}
+                </ul>
+              )}
+            </li>
           </ul>
         )}
       </Droppable>
